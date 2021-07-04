b8db7b687ba22044d17b7ad55d2f0dc93e6c2a3b
AmelieAlonah <amelie.abdallah@hotmail.fr>
2021-07-04 16:18:05 +0200

    Suppresion

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,7 @@
-import '../css/style.css';
 
 const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+const input = document.querySelector("form > input");
 
 const todos = [
   {
@@ -9,9 +10,16 @@ const todos = [
   },
   {
     text: "faire du javascript",
-    done: false
+    done: true
   }
 ];
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const value = input.value;
+  input.value = "";
+  addTodo(value);
+});
 
 const displayTodo = () => {
   const todosNode = todos.map((todo, index) => {
@@ -23,11 +31,30 @@ const displayTodo = () => {
 
 const createTodoElement = (todo, index) => {
   const li = document.createElement("li");
+  const buttonDelete = document.createElement("button");
+  buttonDelete.innerHTML = "Supprimer";
+  buttonDelete.addEventListener("click", event => {
+    deleteTodo(index);
+  });
   li.innerHTML = `
     <span class="todo ${todo.done ? "done" : ""}"></span>
     <p>${todo.text}</p>
   `;
+  li.appendChild(buttonDelete);
   return li;
+};
+
+const addTodo = text => {
+  todos.push({
+    text,
+    done: false
+  });
+  displayTodo();
+};
+
+const deleteTodo = index => {
+  todos.splice(index, 1);
+  displayTodo();
 };
 
 displayTodo();
